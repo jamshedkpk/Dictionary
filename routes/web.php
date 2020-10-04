82e854d386891adf03686eccf0815d65e218bd38
jamshedkpk <jamshedkpk@hotmail.com>
2020-10-04 02:58:31 -0700

    Dictionary

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,4 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\WordController;
 Route::get('/',[AdminController::class, 'index']);
 Route::resource('Word',WordController::class);
-use App\Http\Controllers\UserController;
-Route::get('/users', [UserController::class, 'index'])->name('users.index');
+
